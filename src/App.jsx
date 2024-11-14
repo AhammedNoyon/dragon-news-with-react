fe8fa0f1de4ceaea5c05086932f4226ab279e19f
AhammedNoyon <mdnoyon956993@gmail.com>
2024-11-14 21:28:16 +0600

    Initialized the dragon news with react project with basic structure done

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h3 className="text-5xl">test tailwind css </h3>
+    </>
+  );
+}
+
+export default App;
---
+++ src/layout/Layout.jsx
@@ -0,0 +1,5 @@
+const Layout = () => {
+  return <div className="w-11/12 md:w-3/4 mx-auto">Layout</div>;
+};
+
+export default Layout;
